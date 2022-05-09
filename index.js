8ebfaf53fdc17366c0f148cf7fb623a5b433cb45
Praveen Upadhyay <praveenupadhyay997@gmail.com>
2022-05-09 17:07:34 +0530

    [Praveen] Number divisible by 11.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log("Feeling Proud")
+
+var numericObject = [54, 69, 22, 33, 5, 10, 19, 45, 36, 46, 84, 90, 200, 200];
+let stringObject = ["Saab", "Volvo", "BMW", "Perl", "Mohtashim", "zara", "uliptart"];
+
+function isEven(x)
+{
+    return x%11 == 0;
+}
+console.log(numericObject.filter(x => isEven(x)));
